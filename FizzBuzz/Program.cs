85e36766f3f957769570e1d1f28b0d19401f7825
Hema Nalabanda <Hema.Nalabanda@softwire.com>
2020-02-03 15:39:06 +0000

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (IsMultipleOf3(i))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (IsMultipleOf5(i))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        
+        static Boolean IsMultipleOf3(int i)
+        {
+            if (i%3>=1)
+            {
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+        static Boolean IsMultipleOf5(int i)
+        {
+            if (i%5>=1)
+            {
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
